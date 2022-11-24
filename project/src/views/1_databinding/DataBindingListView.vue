5b2d106e0b9048ef8313521778511645248a58d3
kim-kyuho-dev <kim_kyuho@ss-group.jp>
2022-11-24 17:46:05 +0900

    Add Class DataBinding

--- project/src/views/1_databinding/DataBindingListView.vue
+++ project/src/views/1_databinding/DataBindingListView.vue
@@ -7,9 +7,30 @@
             </option>
         </select>
     </div>
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Name</th>
+                    <th>Price</th>
+                    <th>Quantity</th>
+                    <th>Total</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr :key="drink.drinkId" v-for = "drink in drinkList">
+                    <td> {{ drink.drinkId }}</td>
+                    <td> {{ drink.drinkName }}</td>
+                    <td> {{ drink.price }}</td>
+                    <td><input type="number" name="" id="" v-model="drink.qty"></td>
+                    <td>{{ drink.price * drink.qty }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 <script>
-
 export default {
     components: {},
     data () {
@@ -18,6 +39,38 @@ export default {
                 { title: 'tokyo', code: '01' },
                 { title: 'osaka', code: '02' },
                 { title: 'nagoya', code: '03' }
+            ],
+            drinkList: [
+                {
+                    drinkId: '1',
+                    drinkName: 'Cola',
+                    price: 700,
+                    qty: 1
+                },
+                {
+                    drinkId: '2',
+                    drinkName: 'Cidar',
+                    price: 700,
+                    qty: 1
+                },
+                {
+                    drinkId: '3',
+                    drinkName: 'Lemonade',
+                    price: 800,
+                    qty: 1
+                },
+                {
+                    drinkId: '4',
+                    drinkName: 'MelonSoda',
+                    price: 600,
+                    qty: 1
+                },
+                {
+                    drinkId: '5',
+                    drinkName: 'GingerAle',
+                    price: 850,
+                    qty: 1
+                }
             ]
         }
     },
